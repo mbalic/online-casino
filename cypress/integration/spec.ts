3525111987989728df256be435433ed04a69cc7d
Mario Balić <mario.balic1@gmail.com>
2022-02-22 17:51:43 +0100

    added e2e tests

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,7 +1,13 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
-  })
-})
+describe('Tests', () => {
+  it('Check if page loads', () => {
+    cy.visit('/');
+    cy.get('ul.navbar-nav li.nav-item.active a.nav-link').contains('Top games');
+    cy.get('.container .games-list .game-item .game-name').contains('The Wish Master');
+  });
+
+  it('Check if navigation works', () => {
+    cy.visit('/');
+    cy.get('a.nav-link').last().click();
+    cy.get('.container .games-list .game-item .game-name').contains('Studio1 Bingo');
+  });
+});
