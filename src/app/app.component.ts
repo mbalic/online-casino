41a126f3d438621b1e1887b445fc908a34490664
Mario Balić <mario.balic1@gmail.com>
2022-02-22 11:23:56 +0100

    set page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { interval } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs/operators';
 import { GameFacade } from './facades/game.facade';
 
 @Component({
@@ -8,9 +11,36 @@ import { GameFacade } from './facades/game.facade';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private gameFacade: GameFacade) {}
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
+    private gameFacade: GameFacade
+  ) {}
 
   ngOnInit(): void {
+    this.setPageTitle();
+    this.loadData();
+  }
+
+  setPageTitle() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map((route) => {
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        filter((route) => route.outlet === 'primary'),
+        mergeMap((route) => route.data)
+      )
+      .subscribe((event) => {
+        this.titleService.setTitle(`${event['title']} - Online casino`);
+      });
+  }
+
+  loadData() {
     this.gameFacade.loadGames();
 
     interval(3000).subscribe(() => {
